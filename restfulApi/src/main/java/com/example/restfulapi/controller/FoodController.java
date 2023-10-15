6dab23ac94abf23cfe717a9e4538655f2efbd68a
WoongKi1115 <nwk6638@gmail.com>
2023-10-16 08:27:14 +0900

    feat: poi 활용 data 출력 확인

--- restfulApi/src/main/java/com/example/restfulapi/controller/FoodController.java
+++ restfulApi/src/main/java/com/example/restfulapi/controller/FoodController.java
@@ -2,8 +2,13 @@ package com.example.restfulapi.controller;
 
 import com.example.restfulapi.service.FoodService;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/")
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FoodController {
     private final FoodService foodService;
 
+    @PostMapping
+    ResponseEntity<?> insertData() throws IOException, InvalidFormatException {
+        foodService.insertData();
+        return (ResponseEntity<?>) ResponseEntity.ok();
+    }
 }
--- restfulApi/src/main/java/com/example/restfulapi/service/FoodService.java
+++ restfulApi/src/main/java/com/example/restfulapi/service/FoodService.java
@@ -1,13 +1,36 @@
 package com.example.restfulapi.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ResourceLoader;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 //@Transactional(readOnly = true)
 public class FoodService {
+    public void insertData() throws IOException, InvalidFormatException {
+        ClassPathResource resource = new ClassPathResource("data.xlsx");
+        File file = resource.getFile();
 
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        // 각 행을 반복하며 데이터를 읽음
+        for (Row row : sheet) {
+            // 셀을 반복하며 데이터를 읽음
+            for (Cell cell : row) {
+                // 셀 값 출력
+                System.out.print(cell.toString() + "\t");
+            }
+            System.out.println(); // 다음 행으로 이동
+        }
+
+    }
 }
